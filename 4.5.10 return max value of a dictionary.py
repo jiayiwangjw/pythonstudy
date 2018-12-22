91b181e83af7085d09a12f29fd1b1b70dcc4f130
jiayiwangjw <JIAYI0206@ME.COM>
2018-12-22 15:52:47 -0500

    Update 4.5.10 return max value of a dictionary.py

--- 4.5.10 return max value of a dictionary.py
+++ 4.5.10 return max value of a dictionary.py
@@ -25,53 +25,37 @@ solution 2
 """
 def most_oscars(oscar_dict):
     
-    #There are several slightly different ways we could
-    #do this. This is mine, but yours might differ.
+    #There are several slightly different ways we could do this. This is mine, but yours might differ.
     #
-    #At a high level, we want to keep track of who has
-    #the most nominations of the people we've looked at
-    #so far, and update that if we encounter someone
-    #with more nominations. So, we need to keep track
-    #of a name...
+    #At a high level, we want to keep track of who has the most nominations of the people we've looked at
+    #so far, and update that if we encounter someone with more nominations. So, we need to keep track of a name...
     
     current_max_name = ""
     
-    #...and a number. However, we can easily look up
-    #the number associated with the current name in the
-    #dictionary, so I'm not going to have a separate
-    #variable for that. You'll see why shortly.
+    #...and a number. However, we can easily look up the number associated with the current name in the
+    #dictionary, so I'm not going to have a separate variable for that. You'll see why shortly.
     #
-    #So, now we need to iterate through all the keys
-    #in the dictionary. We could also go ahead and
-    #iterate over the values, but let's stick to just
-    #the keys:
+    #So, now we need to iterate through all the keys in the dictionary. We could also go ahead and
+    #iterate over the values, but let's stick to just the keys:
     
     for person in oscar_dict:
         
-        #Now we want to check if this current person
-        #has more Oscars than the current maximum
-        #we've found. The only caveat is that if this
-        #is the first person we've checked, we want to
-        #copy their name in no matter what. So, we do
-        #this:
+        #Now we want to check if this current person has more Oscars than the current maximum
+        #we've found. The only caveat is that if this is the first person we've checked, we want to
+        #copy their name in no matter what. So, we do this:
         #
-        #If this is the first person, set this person
-        #as our current_max_person:
+        #If this is the first person, set this person as our current_max_person:
         
         if current_max_person == "":
             current_max_person = person
             
-        #Otherwise, if this is not the first person,
-        #but this person has more nominations than the
-        #current max person, set this person as our
-        #current_max_person:
+        #Otherwise, if this is not the first person, but this person has more nominations than the
+        #current max person, set this person as our current_max_person:
         elif oscar_dict[current_max_person] < oscar_dict[person]:
             current_max_person = person
     
-    #When that loop is done, current_max_person will be
-    #the key in the dictionary that corresponds to the
-    #largest value. So, we can return that key and its
-    #value from the dictionary:
+    #When that loop is done, current_max_person will be the key in the dictionary that corresponds to the
+    #largest value. So, we can return that key and its value from the dictionary:
     
     return (current_max_person, oscar_dict[current_max_person])
 
