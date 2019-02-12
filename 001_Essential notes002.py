97d83d520149fba45eb926fe9a6960d4c7cbeae9
jiayiwangjw <JIAYI0206@ME.COM>
2019-02-12 14:24:51 -0500

    Update 001_Essential notes002.py

--- 001_Essential notes002.py
+++ 001_Essential notes002.py
@@ -36,3 +36,30 @@ def flatten(L):
         else:
             flatList += flatten(i)  #use recursion
     return flatList   
+
+
+##1.3-more Python exercises -- problem05_Pair Counts
+"""
+003 找出组合是数字连续的个数
+"""
+#L1=[1,2,3,4,5,6,7,8,9]
+#L2=[1,1,1,2,2,3,4,10]
+#L3=[1,4,7,9]
+#L4=[]
+#    assert count_pairs(L1)==8, "Test Case L1 failed"   1，2  2，3  3，4  4，5  5，6  6，7  7，8  8，9
+#    assert count_pairs(L2)==9, "Test Case L2 failed"   1，2  1，2  1，2  1，2  1，2  1，2  2，3，2，3，3，4
+#    assert count_pairs(L3)==0, "Test Case L3 failed"
+#    assert count_pairs(L4)==0, "Test Case L4 failed"
+
+
+from collections import Counter
+def count_pairs(L):   #L [1, 1, 1, 2, 2, 5, 8, 8]
+    assert type(L)==list
+    counts = Counter(L)    #counts   ({1: 3, 2: 2, 5: 1, 8: 2})
+    unique_items = sorted(counts.keys())  #[1, 2, 5, 8]
+    unique_pairs = zip(unique_items[1:], unique_items[:-1])   #zip([2, 5, 8], [1, 2, 5])  -->[(2, 1), (5, 2), (8, 5)]
+    unit_diff_combos = [counts[b]*counts[a] for b, a in unique_pairs if (b-a) == 1]   #[6]
+    return sum(unit_diff_combos)
+    
+
+
