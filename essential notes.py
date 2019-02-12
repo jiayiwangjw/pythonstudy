e7d2d26e2214a27853c1c16301e0923e899e129f
jiayiwangjw <JIAYI0206@ME.COM>
2019-02-12 10:48:36 -0500

    Update essential notes.py

--- essential notes.py
+++ essential notes.py
@@ -24,7 +24,8 @@ grade_lists = {}
 for i in grades[1:]:
     grade_lists[i[0]] = [int(g) for g in i[1:]]
 
-#{'Foster': [89, 97, 101], 'Rabbit': [59, 61, 67], 'Mac': [88, 99, 111], 'Farva': [45, 56, 67], 'Ursula': [73, 79, 83], 'Thorny': [100, 90, 80]}
+#{'Foster': [89, 97, 101], 'Rabbit': [59, 61, 67], 'Mac': [88, 99, 111], 'Farva': [45, 56, 67], 'Ursula': [73, 79, 83], 
+#'Thorny': [100, 90, 80]}
 
 
 
@@ -38,7 +39,9 @@ grade_dicts = {}
 for L in grades[1:]:
     grade_dicts[L[0]] = dict(zip(assignments, [int(g) for g in L[1:]]))
 
-#{'Foster': {'Exam 1': 89, 'Exam 2': 97, 'Exam 3': 101}, 'Rabbit': {'Exam 1': 59, 'Exam 2': 61, 'Exam 3': 67}, 'Mac': {'Exam 1': 88, 'Exam 2': 99, 'Exam 3': 111}, 'Farva': {'Exam 1': 45, 'Exam 2': 56, 'Exam 3': 67}, 'Ursula': {'Exam 1': 73, 'Exam 2': 79, 'Exam 3': 83}, 'Thorny': {'Exam 1': 100, 'Exam 2': 90, 'Exam 3': 80}}
+#{'Foster': {'Exam 1': 89, 'Exam 2': 97, 'Exam 3': 101}, 'Rabbit': {'Exam 1': 59, 'Exam 2': 61, 'Exam 3': 67}, 
+#'Mac': {'Exam 1': 88, 'Exam 2': 99, 'Exam 3': 111}, 'Farva': {'Exam 1': 45, 'Exam 2': 56, 'Exam 3': 67}, 
+#'Ursula': {'Exam 1': 73, 'Exam 2': 79, 'Exam 3': 83}, 'Thorny': {'Exam 1': 100, 'Exam 2': 90, 'Exam 3': 80}}
 
 
 
@@ -47,9 +50,10 @@ for L in grades[1:]:
 """
 #avg_grades_by_student['Mac'] - 99.333333333
 from statistics import mean
-avg_grades_by_student = {n:mean(g) for n, g in grade_lists.items() }
+avg_grades_by_student = {n:mean(g) for n, g in grade_lists.items() }  #grade_lists {'Foster': [89, 97, 101],...}
 
-#{'Foster': 95.66666666666667, 'Rabbit': 62.333333333333336, 'Mac': 99.33333333333333, 'Farva': 56, 'Ursula': 78.33333333333333, 'Thorny': 90}   
+#{'Foster': 95.66666666666667, 'Rabbit': 62.333333333333336, 'Mac': 99.33333333333333, 'Farva': 56, 
+#'Ursula': 78.33333333333333, 'Thorny': 90}   
     
     
     
