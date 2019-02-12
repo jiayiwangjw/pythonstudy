b5ee5044ff4386b7ae88b5cef5882cc37c641f41
jiayiwangjw <JIAYI0206@ME.COM>
2019-02-12 10:37:38 -0500

    Update essential notes.py

--- essential notes.py
+++ essential notes.py
@@ -1,4 +1,4 @@
-#选取List of List的一行
+##001 选取List of List的一行
 grades = [
     ['Student', 'Exam 1', 'Exam 2', 'Exam 3'],
     ['Thorny', '100', '90', '80'],
@@ -10,3 +10,18 @@ grades = [
 ]
 
 students = [i[0] for i in grades[1:]]  #['Thorny', 'Mac', 'Farva', 'Rabbit', 'Ursula', 'Foster']
+
+##002 创建一个字典dict, 列值为key
+#grade_lists['Mac'] == [88, 99, 111]
+#grade_lists['Ursula'] == [73, 79, 83]
+grade_lists = {}
+for i in grades[1:]:
+    grade_lists[i[0]] = [int(g) for g in i[1:]]
+
+##003 创建一个字典dict, 列值行值同时为key
+#grade_dicts['Foster']['Exam 1'] == 89
+#grade_dicts['Rabbit']['Exam 3'] == 67
+assignments = grades[0][1:4]  #assignments == ['Exam 1', 'Exam 2', 'Exam 3']
+grade_dicts = {}
+for L in grades[1:]:
+    grade_dicts[L[0]] = dict(zip(assignments, [int(g) for g in L[1:]]))
